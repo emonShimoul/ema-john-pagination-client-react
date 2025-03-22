d3049e768ff4ee8d9ae22f274e4f24233079cd33
emonShimoul <emonshimoul@gmail.com>
2025-03-22 23:53:39 +0600

    Create dynamic pagination buttons

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -96,6 +96,12 @@ const Shop = () => {
           </Link>
         </Cart>
       </div>
+
+      <div className="pagination">
+        {pages.map((page) => (
+          <button key={page}>{page}</button>
+        ))}
+      </div>
     </div>
   );
 };
